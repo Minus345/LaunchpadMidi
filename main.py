4959ab73577e308ce9609a474330ba9484f6e43f
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-10-21 01:19:49 +0200

    update toggle buttons to normal buttons

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import yaml
 import gui
 from createConfig import createConfig
 
-global outputLaunchaPad, inputLaunchPad, outputToSoftware, inputFromSoftware, faders, button, notes, percentage, flash, switchOn, savePercentage, faderColour, queue
+global outputLaunchaPad, inputLaunchPad, outputToSoftware, inputFromSoftware, faders, flashButton, notes, percentage, flash, switchOn, savePercentage, faderColour, queue
 
 
 def checkStart():
@@ -29,15 +29,14 @@ def updateLightingInColum(colum):
     # notes = button
     # velocity = colour
     # channel = static, flashing, pulsing
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][1], velocity=flashButton[colum][0], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][0], velocity=71, channel=0))
 
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][0], velocity=button[colum][0], channel=2))
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][1], velocity=button[colum][1], channel=0))
-
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][3], velocity=faders[colum][0], channel=0))
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][4], velocity=faders[colum][1], channel=0))
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][5], velocity=faders[colum][2], channel=0))
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][6], velocity=faders[colum][3], channel=0))
-    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][7], velocity=faders[colum][4], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][2], velocity=faders[colum][0], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][3], velocity=faders[colum][1], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][4], velocity=faders[colum][2], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][5], velocity=faders[colum][3], channel=0))
+    outputLaunchaPad.send(mido.Message('note_on', note=notes[colum][6], velocity=faders[colum][4], channel=0))
 
 
 def updateColumWithPercentage(percentage, flash, colum):
@@ -75,9 +74,9 @@ def updateColumWithPercentage(percentage, flash, colum):
             faders[colum][0] = color
 
     if flash[colum]:
-        button[colum][1] = 21
+        flashButton[colum][0] = 21
     else:
-        button[colum][1] = 45
+        flashButton[colum][0] = 45
 
 
 def sendMidiToSoftware(percentage, colum):
@@ -95,28 +94,6 @@ def sendMidiToSoftware(percentage, colum):
 
 
 def getButtons(message, y):
-    # faders
-    if message == mido.Message("note_on", note=notes[y][0], velocity=127, channel=0):
-        # toggle button
-        if not switchOn[y]:
-            switchOn[y] = True
-        else:
-            switchOn[y] = False
-
-        if switchOn[y]:
-            button[y][0] = 45
-            outputToSoftware.send(mido.Message('note_on', note=y + 1, velocity=127, channel=0))
-            updateLightingInColum(y)
-            return
-        else:
-            button[y][0] = 0
-            outputToSoftware.send(mido.Message('note_off', note=y + 1, velocity=127, channel=0))
-            updateLightingInColum(y)
-            return
-
-    if message == mido.Message("note_on", note=notes[y][0], velocity=0, channel=0):  # catch if toggle is released
-        return
-
     if message == mido.Message("note_on", note=notes[y][1], velocity=127, channel=0):  # flash
         savePercentage[y] = percentage[y]
         percentage[y] = 100
@@ -127,15 +104,15 @@ def getButtons(message, y):
             percentage[y] = savePercentage[y]
             flash[y] = False
 
-    if message == mido.Message("note_on", note=notes[y][3], velocity=127, channel=0):
+    if message == mido.Message("note_on", note=notes[y][2], velocity=127, channel=0):
         percentage[y] = 0
-    if message == mido.Message("note_on", note=notes[y][4], velocity=127, channel=0):
+    if message == mido.Message("note_on", note=notes[y][3], velocity=127, channel=0):
         percentage[y] = 25
-    if message == mido.Message("note_on", note=notes[y][5], velocity=127, channel=0):
+    if message == mido.Message("note_on", note=notes[y][4], velocity=127, channel=0):
         percentage[y] = 50
-    if message == mido.Message("note_on", note=notes[y][6], velocity=127, channel=0):
+    if message == mido.Message("note_on", note=notes[y][5], velocity=127, channel=0):
         percentage[y] = 75
-    if message == mido.Message("note_on", note=notes[y][7], velocity=127, channel=0):
+    if message == mido.Message("note_on", note=notes[y][6], velocity=127, channel=0):
         percentage[y] = 100
 
     updateColumWithPercentage(percentage, flash, y)
@@ -155,13 +132,26 @@ def loop(message):
             outputToSoftware.send(mido.Message('note_off', note=10 + x, velocity=127, channel=0))
             outputLaunchaPad.send(mido.Message('control_change', channel=0, control=control[x], value=0))
             return
+
+    # bottom buttons
+    bottomButtons = [11, 12, 13, 14, 15, 16, 17, 18]
+    for x in range(len(bottomButtons)):
+        if message == mido.Message("note_on", note=bottomButtons[x], velocity=127, channel=0):
+            outputToSoftware.send(mido.Message('note_on', note=x + 1, velocity=127, channel=0))
+            outputLaunchaPad.send(mido.Message('note_on', note=bottomButtons[x], velocity=3, channel=0))
+            return
+        if message == mido.Message("note_on", note=bottomButtons[x], velocity=0, channel=0):
+            outputToSoftware.send(mido.Message('note_off', note=x + 1, velocity=127, channel=0))
+            outputLaunchaPad.send(mido.Message('note_off', note=bottomButtons[x], velocity=3, channel=0))
+            return
+
     # check wich channel is pressed:
-    intensity = ["toggle", "flash", "not used", "0", "25", "50", "75", "100"]
-    for x in range(8):  # x is colum  y is row y: 0: toggle 1: flash 2: not used 3:0% 4:25% 5:50% 6:75% 7:100%
+    intensity = ["flash", "not used", "0", "25", "50", "75", "100"]
+    for x in range(7):  # x is colum  y is row y: 0: toggle 1: flash 2: not used 3:0% 4:25% 5:50% 6:75% 7:100%
         for y in range(8):
             if message == mido.Message("note_on", note=notes[y][x], velocity=127, channel=0) or message == mido.Message(
                     "note_on", note=notes[y][x], velocity=0, channel=0):
-                # print("colum: ", y, "row: ", intensity[x], message)
+                print("colum: ", y, "row: ", intensity[x], message)
                 getButtons(message, y)
 
 
@@ -205,7 +195,7 @@ def startMidi(q):
         configFile = yaml.safe_load(file)
 
     print("starting Midi")
-    global outputLaunchaPad, inputLaunchPad, outputToSoftware, inputFromSoftware, faders, button, notes, percentage, flash, switchOn, savePercentage
+    global outputLaunchaPad, inputLaunchPad, outputToSoftware, inputFromSoftware, faders, flashButton, notes, percentage, flash, switchOn, savePercentage
 
     outputLaunchaPad = mido.open_output(configFile['outputLaunchaPad'])
     inputLaunchPad = mido.open_input(configFile['inputLaunchPad'])
@@ -215,10 +205,10 @@ def startMidi(q):
     faders = [[50, 50, 50, 50, 50], [50, 50, 50, 50, 50], [50, 50, 50, 50, 50], [50, 50, 50, 50, 50],
               [50, 50, 50, 50, 50], [50, 50, 50, 50, 50],
               [50, 50, 50, 50, 50], [50, 50, 50, 50, 50]]
-    button = [[0, 45], [0, 45], [0, 45], [0, 45], [0, 45], [0, 45], [0, 45], [0, 45]]
-    notes = [[11, 21, 31, 41, 51, 61, 71, 81], [12, 22, 32, 42, 52, 62, 72, 82], [13, 23, 33, 43, 53, 63, 73, 83],
-             [14, 24, 34, 44, 54, 64, 74, 84], [15, 25, 35, 45, 55, 65, 75, 85], [16, 26, 36, 46, 56, 66, 76, 86],
-             [17, 27, 37, 47, 57, 67, 77, 87], [18, 28, 38, 48, 58, 68, 78, 88]]
+    flashButton = [[45, 45], [45, 45], [45, 45], [45, 45], [45, 45], [45, 45], [45, 45], [45, 45]]
+    notes = [[21, 31, 41, 51, 61, 71, 81], [22, 32, 42, 52, 62, 72, 82], [23, 33, 43, 53, 63, 73, 83],
+             [24, 34, 44, 54, 64, 74, 84], [25, 35, 45, 55, 65, 75, 85], [26, 36, 46, 56, 66, 76, 86],
+             [27, 37, 47, 57, 67, 77, 87], [28, 38, 48, 58, 68, 78, 88]]
 
     percentage = [0, 0, 0, 0, 0, 0, 0, 0]
     flash = [False, False, False, False, False, False, False, False]
